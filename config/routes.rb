71d0a7abe3ae8a9e61e4720c57b72461fa78ce26
luanvd <vuvanlyth@gmail.com>
2018-12-25 17:19:22 +0700

    Add base layout admin / Add gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home_pages#index"
+
+  namespace :admin do 
+  	root "users#index"
+  	resource :users
+  end
 end
