08572b49508d6c7e4c91205e259297f2632eed95
luanvd <vuvanlyth@gmail.com>
2018-12-26 13:33:27 +0700

    Login, Logout

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,15 @@
 class AdminController < ApplicationController   
-  layout "admin" 
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for :account_update do |u|
+      u.permit :name, :email, :user_avatar, :password, :password_confirmation, :current_password
+    end
+
+    devise_parameter_sanitizer.for :sign_up do |u|
+      u.permit :name, :email, :user_avatar, :password, :password_confirmation
+    end
+  end
 end
