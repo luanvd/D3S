11661334e6c0fbd75906a401b4ae02086e13a602
cuongnc0211 <cuongnguyenfu@gmail.com>
2018-12-26 21:59:00 +0700

    base I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home_pages#index"
+  scope "(:locale)", locale: /en|fr/ do
+    root "home_pages#index"
 
-  namespace :admin do
-  	root :to => "users#index"
-  	resources :users
-  	resources :sliders
+    namespace :admin do
+      root :to => "users#index"
+      resources :users
+      resources :sliders
+    end
   end
 end
