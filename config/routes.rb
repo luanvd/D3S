a2b608cc7b3a0c323ba1dba67ffb0f9ee8e97e8d
luanvd <vuvanlyth@gmail.com>
2019-01-06 23:06:37 +0700

    Process form, send email

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope "(:locale)", locale: /en|fr/ do
     root "home_pages#index"
+    resources :customers
 
     namespace :admin do
       root :to => "users#index"
