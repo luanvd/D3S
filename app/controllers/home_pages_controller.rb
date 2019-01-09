44b6258d447a60331e47cbf8dcd9cb9d08682be7
anhvt <vutienanh1989@gmail.com>
2019-01-09 11:07:33 +0700

    Show origine

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,8 +1,16 @@
 class HomePagesController < ApplicationController
+  before_action :load_origines, only: :index
 
   def index
     @customer = Customer.new
+
     @slider_show = SliderShow.first
     @slider_images = @slider_show.slider_images
+  end
+
+  private
+  def load_origines
+    @origine = Origine.first
+    @origine_language = @origine.origine_languages.find_by_language(locale)
   end
 end
